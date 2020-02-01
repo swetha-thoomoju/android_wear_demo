18754c7bb1dc241e79442614a17c5b7acb5f9486
Swetha Thoomoju <swetha.thoomoju@cariboutech.com>
2020-02-02 03:55:09 +0530

    initial commit of empty activity

---
+++ app/src/main/java/io/swetha/wear_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package io.swetha.wear_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
